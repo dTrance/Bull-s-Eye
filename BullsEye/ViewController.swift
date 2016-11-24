5b6a66ac1a19466d4dbe21cb392726c781ed5b8f
Doug Trance <dtrance@dtcc.edu>
2016-11-24 09:18:15 -0500

    Updated UI to support 3.5 inch iphone

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -25,22 +25,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         startNewGame()
         updateLabels()
-        
-        let thumbImageNormal = #imageLiteral(resourceName: "SliderThumb-Normal")
-        slider.setThumbImage(thumbImageNormal, for: .normal)
-        
-        let thumbImageHighlighted = #imageLiteral(resourceName: "SliderThumb-Highlighted")
-        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
-        
-        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
-        
-        let trackLeftImage = #imageLiteral(resourceName: "SliderTrackLeft")
-        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
-        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
-        
-        let trackRightImage = #imageLiteral(resourceName: "SliderTrackRight")
-        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
-        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+        sliderTracking()
         
     }
 
@@ -113,6 +98,25 @@ class ViewController: UIViewController {
     @IBAction func startOver() {
         startNewGame()
         updateLabels()
+    }
+    
+    func sliderTracking() {
+        let thumbImageNormal = #imageLiteral(resourceName: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        
+        let thumbImageHighlighted = #imageLiteral(resourceName: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        let trackLeftImage = #imageLiteral(resourceName: "SliderTrackLeft")
+        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = #imageLiteral(resourceName: "SliderTrackRight")
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+
     }
 }
 
